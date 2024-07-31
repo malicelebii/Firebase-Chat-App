e4eff46b128ad53e031cee7397d76fc7b7e11df4
malicelebii <mehmetalicelebii@gmail.com>
2024-07-31 12:21:37 +0300

    + Setup textfields delegates

--- Firebase-Chat-App/Modules/Register/RegisterViewController/RegisterViewController.swift
+++ Firebase-Chat-App/Modules/Register/RegisterViewController/RegisterViewController.swift
@@ -103,14 +103,12 @@ class RegisterViewController: UIViewController {
         addSubViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupDelegates() {
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
     
     func addSubViews() {
         view.addSubview(scrollView)
