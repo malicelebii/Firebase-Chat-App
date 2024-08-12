eea1570917fd042bb23c3a814ef9c60b1052947c
malicelebii <mehmetalicelebii@gmail.com>
2024-08-12 08:36:10 +0300

    + Create ChatAppUser Model to store user infos in database

---
+++ Firebase-Chat-App/Models/ChatAppUser.swift
@@ -0,0 +1,20 @@
+//
+//  ChatAppUser.swift
+//  Firebase-Chat-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 12.08.2024.
+//
+
+import Foundation
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let email: String
+    
+    var safeEmail : String {
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+}
