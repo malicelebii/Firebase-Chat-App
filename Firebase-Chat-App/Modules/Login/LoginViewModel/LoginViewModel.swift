a119ed6a83c7f82002a0d1739f9ead073c0ca4d6
malicelebii <mehmetalicelebii@gmail.com>
2024-08-13 08:30:17 +0300

    + Create loginWithFB method to sign in via FB

--- Firebase-Chat-App/Modules/Login/LoginViewModel/LoginViewModel.swift
+++ Firebase-Chat-App/Modules/Login/LoginViewModel/LoginViewModel.swift
@@ -10,6 +10,7 @@ import FirebaseAuth
 
 protocol LoginViewModelDelegate {
     func login(withEmail: String, password: String)
+    func loginWithFB(token: String)
 }
 
 final class LoginViewModel: LoginViewModelDelegate {
@@ -24,5 +25,32 @@ final class LoginViewModel: LoginViewModelDelegate {
         }
     }
     
-    
+    func loginWithFB(token: String) {
+        let fbRequest = FBSDKLoginKit.GraphRequest(graphPath: "me", parameters: ["fields": "email, name"], tokenString: token, version: nil, httpMethod: .get)
+        fbRequest.start(completion: { _, result, error in
+            guard let result = result as? [String: Any], error == nil else { print("Failed to make fb graph request"); return }
+          
+            let credential = FacebookAuthProvider.credential(withAccessToken:  AccessToken.current!.tokenString)
+            
+            guard let userName = result["name"] as? String, let email = result["email"] as? String else { print("Failed to get email and name from fb result"); return }
+            let nameComponents = userName.components(separatedBy: " ")
+            
+            self.databaseManager.userExist(with: email) { exist in
+                if !exist {
+                    self.databaseManager.insertUser(with: ChatAppUser(firstName: nameComponents[0], lastName: nameComponents[1], email: email))
+                }
+            }
+            
+            FirebaseAuth.Auth.auth().signIn(with: credential) { [weak self] authResult, error in
+                guard let self = self else { return }
+                guard authResult != nil, error == nil else {
+                    print("FB credential login failed, MFA may be needed \(error)")
+                    print(credential)
+                    return
+                }
+                self.view?.didLogin()
+                print("Successfully logged in ")
+            }
+        })
+    }
 }
