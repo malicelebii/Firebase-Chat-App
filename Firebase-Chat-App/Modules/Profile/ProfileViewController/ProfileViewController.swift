861797feb7a79e237bd601ff14a8b84d986d50a0
malicelebii <mehmetalicelebii@gmail.com>
2024-08-13 08:27:47 +0300

    + TableView initialize

--- Firebase-Chat-App/Modules/Profile/ProfileViewController/ProfileViewController.swift
+++ Firebase-Chat-App/Modules/Profile/ProfileViewController/ProfileViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    let data = ["Log out"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    }
 }
