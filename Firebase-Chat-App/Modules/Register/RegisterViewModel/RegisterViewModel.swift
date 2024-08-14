420a6a859360cd88f1c65b9d0102ebf9fce19ee4
malicelebii <mehmetalicelebii@gmail.com>
2024-08-14 14:30:46 +0300

    + Edit register method

--- Firebase-Chat-App/Modules/Register/RegisterViewModel/RegisterViewModel.swift
+++ Firebase-Chat-App/Modules/Register/RegisterViewModel/RegisterViewModel.swift
@@ -9,16 +9,48 @@ import Foundation
 import FirebaseAuth
 
 protocol RegisterViewModelDelegate {
-    func register(withEmail: String, password: String, firstName: String, lastName: String)
+    func register(withEmail: String, password: String, firstName: String, lastName: String, image: UIImage?, view: UIView)
 }
 
 final class RegisterViewModel: RegisterViewModelDelegate {
     func register(withEmail: String, password: String, firstName: String, lastName: String) {
         FirebaseAuth.Auth.auth().createUser(withEmail: withEmail, password: password) { authResult, error in
+    
+    func register(withEmail: String, password: String, firstName: String, lastName: String, image: UIImage?, view: UIView) {
+        spinner.show(in: view)
+        Auth.auth().createUser(withEmail: withEmail, password: password) { [weak self] authResult, error in
+            print("Self: \(self)")
+            
+            guard let self = self else { return }
+            
             guard authResult != nil, error == nil else {
                 return
             }
-            DatabaseManager.shared.insertUser(with: ChatAppUser(firstName: firstName, lastName: lastName, email: withEmail))
+            
+            let chatUser = ChatAppUser(firstName: firstName, lastName: lastName, email: withEmail)
+            self.databaseManager.insertUser(with: chatUser) { success in
+                print("success: \(success)")
+                if success {
+                    //upload image
+                    print("image: \(image)")
+                    guard let image = image, let data = image.pngData() else {
+                        return
+                    }
+                    
+                    let fileName = chatUser.profilePictureFileName
+                    self.storageManager.uploadProfilePicture(with: data, fileName: fileName) { result in
+                        switch result {
+                        case .success(let downloadUrl):
+                            self.view?.didTapRegister()
+                            self.spinner.dismiss()
+                            print(downloadUrl)
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                }
+                
+            }
         }
     }
 }
