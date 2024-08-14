38bcb94726b2989cdeb35282f4ad39c085bc5297
malicelebii <mehmetalicelebii@gmail.com>
2024-08-14 14:22:47 +0300

    + Create StorageEnum to specify errors while storing the pictures

---
+++ Firebase-Chat-App/Enums/StorageError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkErrors.swift
+//  Firebase-Chat-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 14.08.2024.
+//
+
+import Foundation
+
+enum StorageError: Error {
+    case failedToUpload
+    case failedToGetDownloadUrl
+}
