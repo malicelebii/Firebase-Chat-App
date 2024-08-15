de84f54c0032fbfced15d42db6784376a0d217da
malicelebii <mehmetalicelebii@gmail.com>
2024-08-15 15:52:11 +0300

    + Create database error enum

---
+++ Firebase-Chat-App/Enums/DatabaseError.swift
@@ -0,0 +1,12 @@
+//
+//  DatabaseError.swift
+//  Firebase-Chat-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 15.08.2024.
+//
+
+import Foundation
+
+enum DatabaseError: Error {
+    case failedToFetchUsers
+}
