1c7dca2815a102a576bdb22667c0684a3ba9256a
malicelebii <mehmetalicelebii@gmail.com>
2024-08-16 12:59:57 +0300

    + Extend MessageKind

--- Firebase-Chat-App/Models/Message.swift
+++ Firebase-Chat-App/Models/Message.swift
@@ -20,3 +20,31 @@ struct Sender: SenderType {
     var senderId: String
     var displayName: String
 }
+
+extension MessageKind {
+    var messageKindString: String {
+        switch self {
+       
+        case .text(_):
+            return "text"
+        case .attributedText(_):
+            return "attributed_text"
+        case .photo(_):
+            return "photo"
+        case .video(_):
+            return "video"
+        case .location(_):
+            return "location"
+        case .emoji(_):
+            return "emoji"
+        case .audio(_):
+            return "audio"
+        case .contact(_):
+            return "contact"
+        case .linkPreview(_):
+            return "link_preview"
+        case .custom(_):
+            return "custom"
+        }
+    }
+}
