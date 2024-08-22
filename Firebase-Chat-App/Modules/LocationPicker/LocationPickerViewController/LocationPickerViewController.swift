e6678eb755cf26fd4dc0cc29de40f95126f09e5f
malicelebii <mehmetalicelebii@gmail.com>
2024-08-22 10:09:00 +0300

    + Create LocationPickerViewController to select any location and send it

---
+++ Firebase-Chat-App/Modules/LocationPicker/LocationPickerViewController/LocationPickerViewController.swift
@@ -0,0 +1,65 @@
+//
+//  LocationPickerViewController.swift
+//  Firebase-Chat-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 22.08.2024.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class LocationPickerViewController: UIViewController {
+    let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
+    var completion: ((CLLocationCoordinate2D) -> Void)?
+    var coordinates: CLLocationCoordinate2D?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pick Location"
+        view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendButtonTapped))
+        addSubviews()
+        addGestureToMap()
+    }
+    
+    func addSubviews() {
+        view.addSubview(map)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = view.bounds
+    }
+    
+    func addGestureToMap() {
+        map.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTappedMap(_:)))
+        gesture.numberOfTouchesRequired = 1
+        gesture.numberOfTapsRequired = 1
+        map.addGestureRecognizer(gesture)
+    }
+    
+    @objc func sendButtonTapped() {
+        guard let coordinates = coordinates else { return }
+        navigationController?.popViewController(animated: true)
+        completion?(coordinates)
+    }
+    
+    @objc func didTappedMap(_ gesture: UITapGestureRecognizer) {
+        let locationInView = gesture.location(in: map)
+        let coordinates = map.convert(locationInView, toCoordinateFrom: map)
+        self.coordinates = coordinates
+        // Drop a pin on that location
+        for annotation in map.annotations {
+            map.removeAnnotation(annotation)
+        }
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinates
+        map.addAnnotation(pin)
+    }
+}
