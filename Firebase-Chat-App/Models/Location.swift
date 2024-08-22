e60305ea0f3b6f73f7b070c071f32e7dc384eeea
malicelebii <mehmetalicelebii@gmail.com>
2024-08-22 10:04:35 +0300

    + Create Location Model

---
+++ Firebase-Chat-App/Models/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  Firebase-Chat-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 22.08.2024.
+//
+
+import Foundation
+import MessageKit
+import CoreLocation
+
+struct Location: LocationItem {
+    var location: CLLocation
+    var size: CGSize
+}
