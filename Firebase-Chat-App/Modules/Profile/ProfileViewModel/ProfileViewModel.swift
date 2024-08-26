4849c970eeeec4842ee1d6f3fa12ed742ff805c0
malicelebii <mehmetalicelebii@gmail.com>
2024-08-26 21:18:55 +0300

    + Add spinner while profile picture loading

--- Firebase-Chat-App/Modules/Profile/ProfileViewModel/ProfileViewModel.swift
+++ Firebase-Chat-App/Modules/Profile/ProfileViewModel/ProfileViewModel.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SDWebImage
+import JGProgressHUD
 
 protocol ProfileViewModelDelegate {
     func setProfilePicture(for path: String, imageView: UIImageView)
@@ -14,6 +15,7 @@ protocol ProfileViewModelDelegate {
 
 final class ProfileViewModel: ProfileViewModelDelegate {
     let storageManager: StorageManagerDelegate
+    let spinner = JGProgressHUD()
     
     init(storageManager: StorageManagerDelegate = StorageManager.shared) {
         self.storageManager = storageManager
@@ -22,10 +24,13 @@ final class ProfileViewModel: ProfileViewModelDelegate {
 
 extension ProfileViewModel {
     func setProfilePicture(for path: String, imageView: UIImageView) {
+        spinner.show(in: imageView)
         storageManager.downloadURL(for: path) { result in
             switch result {
             case .success(let url):
-                DispatchQueue.main.async {
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.spinner.dismiss()
                     imageView.sd_setImage(with: url)
                 }
             case .failure(let error):
